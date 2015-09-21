60d37cf9e947bd0070481d17c282d99ce8d696eb
Nils Borchers <nilsborchers@gmail.com>
2015-09-21 16:22:03 +0800

    Update 2015-09-21T08:21:58.914Z

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -5,15 +5,21 @@
 
 console.log("--- init nav");
 
-var menuIcon = document.getElementById('menu-icon'),
-    pageMover = document.getElementById('page-mover');
+var menuIcon  = document.getElementById('menu-icon'),
+    pageMover = document.getElementById('page-mover'),
+    siteNav   = document.getElementById('site-nav');
 
 menuIcon.addEventListener('click', function(e) {
     if (hasClass(pageMover, 'moved')) {
         removeClass(pageMover, 'moved');
+
+        setTimeout(function() {
+            removeClass(siteNav, 'showing');
+        }, 500);
     }
 
     else {
         addClass(pageMover, 'moved');
+        addClass(siteNav, 'showing');
     }
 }, false);
