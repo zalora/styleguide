a27b44bdfce35307da5da237acf9692d6a66d4bf
Nils Borchers <nilsborchers@gmail.com>
2015-09-21 17:18:20 +0800

    Update 2015-09-21T09:18:15.278Z

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -5,6 +5,9 @@
 
 console.log("--- init nav");
 
+/**
+ * Toggle menu on mobile devices
+ */
 var menuIcon  = document.getElementById('menu-icon'),
     pageMover = document.getElementById('page-mover'),
     siteNav   = document.getElementById('site-nav');
