db4f2e93194937b4308940cd7ac59256199fb6d3
Nils Borchers <nilsborchers@gmail.com>
2015-10-01 16:20:20 +0800

    Update 2015-10-01T08:20:13.733Z

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,10 +1,31 @@
 /**
+ * Grab extra classnames from below rendered code examples
+ * and add them to example container
+ */
+
+var codeExamples = document.getElementsByClassName('codeExample');
+
+for (i = 0; i < codeExamples.length; i++) {
+    var siblingElement = codeExamples[i].nextElementSibling;
+
+    if (siblingElement && siblingElement.nodeName == "BLOCKQUOTE") {
+        // Grab the classnames from that blockquote element and apply them to the
+        // code example container
+        addClass(codeExamples[i].children[0], siblingElement.children[0].innerHTML);
+
+        // throw away that blockquote that we dont need anymore
+        siblingElement.remove();
+    }
+}
+
+/**
  * Creates iframes on mobile styleguide doc to be able to scope css styles
  */
 var examples = document.querySelectorAll('.exampleOutputMobile'),
     iframeTemplateHead = '\
         <head> \
             <title>I am an iframe</title> \
+            <link rel="stylesheet" href="http://styleguide.zalora.com/assets/css/local/mobile-examples.css"> \
             <link rel="stylesheet" href="http://styleguide.zalora.com/assets/css/mobile/styleguide.css"> \
         </head> \
         <body>';
@@ -15,7 +36,7 @@ for (i = 0; i < examples.length; i++) {
     // create a new iframe to be inserted in the code examples place
     var iframe = document.createElement('iframe'),
         example = examples[i],
-        exampleContent = example.innerHTML,
+        exampleContent = example.outerHTML,
         html = iframeTemplateHead + exampleContent + iframeTemplateFoot;
 
     // Populate the new iframe with content
@@ -28,13 +49,3 @@ for (i = 0; i < examples.length; i++) {
     example.appendChild(iframe);
 }
 
-/**
- * Grab extra classnames from below rendered code examples
- * and add them to example container
- */
-
-var codeExamples = document.getElementsByClassName('codeExample');
-
-for (i = 0; i < codeExamples.length; i++) {
-    console.log(codeExamples[i]).nextElementSibling;
-}
